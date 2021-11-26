a53cea1887c9c220f08721010bfe418c473aaf5e
todashunta <todashuntastem@gmail.com>
2021-11-26 23:07:24 +0900

    11/26 11:00

--- js/header.js
+++ js/header.js
@@ -1,34 +1,8 @@
 const app = Vue.createApp({
     data(){
         return {
-            homeMenu: false,
-            infoMenu: false,
         }
     },
     methods: {
-        homeClick(){
-            this.homeMenu = !this.homeMenu
-            const cover = document.querySelector('.menu-cover')
-            cover.style.height = '100%'
-            if(!this.homeMenu){
-                const cover = document.querySelector('.menu-cover')
-                cover.style.height = '0'
-            }
-        },
-        infoClick(){
-            this.infoMenu = !this.infoMenu
-            const cover = document.querySelector('.menu-cover')
-            cover.style.height = '100%'
-            if(!this.infoMenu){
-                const cover = document.querySelector('.menu-cover')
-                cover.style.height = '0'
-            }
-        },
-        coverclick(){
-            const cover = document.querySelector('.menu-cover')
-            cover.style.height = '0'
-            this.homeMenu = false
-            this.infoMenu = false
-        }
     }
 }).mount('#header')
--- js/slider.js
+++ js/slider.js
@@ -1,7 +1,11 @@
 const slider = Vue.createApp({
     data(){
         return {
-            currentSlide: 1
+            currentSlide: 1,
+            targetWidth: 0,
+            currentTouchX: 0,
+            touchStartX: 0,
+            touchMoveX: 0,
         }
     },
     methods: {
@@ -14,9 +18,8 @@ const slider = Vue.createApp({
             label.click()
         },
         backSlide(){
-            console.log(this.currentSlide)
             this.currentSlide--
-            if(this.currentSlide <= 1 ){
+            if(this.currentSlide <= 0 ){
                 this.currentSlide = 5
             }
             const label = document.getElementById('label' + this.currentSlide)
@@ -24,6 +27,24 @@ const slider = Vue.createApp({
         },
         selectSlide(e){
             this.currentSlide = e.target.id.slice(-1)
+        },
+        swipDown(e) {
+            this.targetWidth = e.target.clientWidth;
+            this.touchStartX = e.targetTouches[0].clientX
+            // console.log(e.targetTouches[0].clientX)
+        },
+        swipMove(e) {
+            this.touchMoveX = this.touchStartX - e.targetTouches[0].clientX
+            // console.log(this.touchMoveX)
+        },
+        swipUp(e) {
+            if (this.targetWidth / 4 <= Math.abs(this.touchMoveX)) {
+                if (this.touchMoveX > 0) {
+                    this.nextSlide()
+                } else {
+                    this.backSlide()
+                }
+            }
         }
     }
 }).mount('#slider')
